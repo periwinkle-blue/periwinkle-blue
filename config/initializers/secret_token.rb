4827a34fbb8fcd340695dc57af4fcdd9b3286c73
Trevor Bowyer <bowyer@gmail.com>
2015-11-29 23:10:19 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeriwinkleBlue::Application.config.secret_key_base = '0f4de79a63ce30bd1219a3f039bde4a154aae6e4ebdaee8963f6fd877cd62ce574f233d2a7d511a6c0f621df86094751808a9c37ab71b8e12584408d55025d67'
