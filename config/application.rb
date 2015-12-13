b9a4097b0bc7984d365d91052c468053c5e3a37e
Brandon <handlez36@gmail.com>
2015-12-13 11:50:34 +0000

    Added model inheritance for individual chess pieces

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module PeriwinkleBlue
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Add pieces sub-directory to load individual chess piece sub-classes
+    config.autoload_paths += %W(#{config.root}/app/models/pieces)
   end
 end
