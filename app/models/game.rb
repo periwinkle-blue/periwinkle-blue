ad47a905b4f746d8da1015ed1c0e73f07ed13e11
Walley Yang <walleyyang@gmail.com>
2015-12-15 14:29:09 +0000

    Created King valid_move?

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,24 +1,24 @@
 class Game < ActiveRecord::Base
   after_create :initialize_board
-  
+
   has_many :pieces
   belongs_to :white_player, class_name: "User"
   belongs_to :black_player, class_name: "User"
-  
+
   def find_piece_icon(x,y)
     piece = self.pieces.where( :x_position => x, :y_position => y).first
     return (!piece.nil?) ? piece.icon.html_safe : ""
   end
-  
+
   # Initialze board with 16 chess pieces to start the game
   def initialize_board
-    
+
     # Create Pawns
     8.times do |index|
       Pawn.create( :x_position => 1, :y_position => index, :color => 0, :game => self)
       Pawn.create( :x_position => 6, :y_position => index, :color => 1, :game => self)
     end
-    
+
     # Create Rooks, Knights, Bishops
     2.times do |index|
       Rook.create( :x_position => 0, :y_position => index * 6 + index, :color => 0, :game => self)
@@ -28,13 +28,13 @@ class Game < ActiveRecord::Base
       Bishop.create( :x_position => 0, :y_position => index * 3 + 2, :color => 0, :game => self)
       Bishop.create( :x_position => 7, :y_position => index * 3 + 2, :color => 1, :game => self)
     end
-    
+
     # Create Kings and Queens
-    King.create( :x_position => 0, :y_position => 3, :color => 0, :game => self)
-    King.create( :x_position => 7, :y_position => 3, :color => 1, :game => self)
-    Queen.create( :x_position => 0, :y_position => 4, :color => 0, :game => self)
-    Queen.create( :x_position => 7, :y_position => 4, :color => 1, :game => self)
-    
+    Queen.create( :x_position => 0, :y_position => 3, :color => 0, :game => self)
+    Queen.create( :x_position => 7, :y_position => 3, :color => 1, :game => self)
+    King.create( :x_position => 0, :y_position => 4, :color => 0, :game => self)
+    King.create( :x_position => 7, :y_position => 4, :color => 1, :game => self)
+
   end
-  
+
 end
