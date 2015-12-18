fccd925a5c4e9342783c5e7cc9623e7d3054d816
Trevor Bowyer <bowyer@gmail.com>
2015-12-18 02:14:53 +0000

    started working on getting the pieces to move, this is for testing only

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,12 @@
 class GamesController < ApplicationController
+
 	def index
 
 	end
 
 	def show
-      @game = Game.find_by_id(params[:id])
-      
+      @game = Game.find_by_id(params[:game_id])
+      @selected_piece = @game.pieces.find_by_id(params[:piece_id])
       @board = [
                   [1, 0, 1, 0, 1, 0, 1, 0],
                   [0, 1, 0, 1, 0, 1, 0, 1],
@@ -16,10 +17,17 @@ class GamesController < ApplicationController
                   [1, 0, 1, 0, 1, 0, 1, 0],
                   [0, 1, 0, 1, 0, 1, 0, 1]
               ]
-      
+              
+      if @selected_piece.present?
+        @selected_piece.update_attributes(:x_position => params[:x], :y_position => params[:y])
+      end 
+
       if @game.nil?
         render :text => "No game specified", :status => :not_found
       end
+
+
 	end
+
   
 end
