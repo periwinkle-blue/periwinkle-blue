4faf682a2a1433ea7600f0cb78a8b405e5b080c3
Walley Yang <walleyyang@gmail.com>
2015-12-23 22:38:05 +0000

    Refactored knight piece

--- app/models/pieces/knight.rb
+++ app/models/pieces/knight.rb
@@ -10,24 +10,11 @@ class Knight < Piece
     x_move = (x_position - x).abs
     y_move = (y_position - y).abs
 
-    # If x or y move is 2 then the next x or y move is 1
-    if x_move == 2 || y_move == 2
-      if x_move == 2
-        # Return true if y is 1
-        return y_move == 1 ? true : false
-      else
-        # Return true if x is 1
-        return x_move == 1 ? true : false
-      end
-    elsif x_move == 1 || y_move == 1
-      if x_move == 1
-        # Return true if y is 2
-        return y_move == 2 ? true : false
-      else
-        # Return true if x is 2
-        return x_move == 2 ? true : false
-      end
-    end
+    return valid_move_combination?(x_move, y_move)
+  end
+
+  def valid_move_combination?(x_move, y_move)
+    return (x_move == 2 && y_move == 1) || (x_move == 1 && y_move ==2) ? true : false
   end
 
 end
--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -8,38 +8,18 @@ RSpec.describe Knight, type: :model do
     @knight = Knight.create(:x_position => 3, :y_position => 3)
   end
 
-  it "should know if x or y move is two then the next x or y move is one vice versa" do
-    # Example move
-    x_top_x = 1
-    x_bottom_x = 5
-    x_left_y = 2
-    x_right_y = 4
+  it "it should allow two moves vertically and then allow only one move horizontally" do
+    expect(@knight.valid_move?(1, 2)).to eq(true)
+    expect(@knight.valid_move?(1, 4)).to eq(true)
+    expect(@knight.valid_move?(5, 2)).to eq(true)
+    expect(@knight.valid_move?(5, 4)).to eq(true)
+  end
 
-    y_top_x = 2
-    y_bottom_x = 4
-    y_left_y = 1
-    y_right_y = 5
-
-    # Calculate old move and new move
-    x_top_x = (@knight.x_position - x_top_x).abs # => 2
-    x_bottom_x = (@knight.x_position - x_bottom_x).abs # => 2
-    x_left_y = (@knight.y_position - x_left_y).abs # => 1
-    x_right_y = (@knight.y_position - x_right_y).abs # => 1
-
-    y_top_x = (@knight.x_position - y_top_x).abs # => 1
-    y_bottom_x = (@knight.x_position - y_bottom_x).abs # => 1
-    y_left_y = (@knight.y_position - y_left_y).abs # => 2
-    y_right_y = (@knight.y_position - y_right_y).abs # => 2
-
-    expect(x_top_x).to be == 2
-    expect(x_bottom_x).to be == 2
-    expect(x_left_y).to be == 1
-    expect(x_right_y).to be == 1
-
-    expect(y_top_x).to be == 1
-    expect(y_bottom_x).to be == 1
-    expect(y_left_y).to be == 2
-    expect(y_right_y).to be == 2
+  it "it should allow two moves horizontally and then allow only one move vertically" do
+    expect(@knight.valid_move?(1, 2)).to eq(true)
+    expect(@knight.valid_move?(1, 4)).to eq(true)
+    expect(@knight.valid_move?(5, 2)).to eq(true)
+    expect(@knight.valid_move?(5, 4)).to eq(true)
   end
 
 end
