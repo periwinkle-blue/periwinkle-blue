c96a421b19f7e65538366537c719e63a3c5656f6
Walley Yang <walleyyang@gmail.com>
2016-01-03 00:38:36 +0000

    Added find_king method

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,29 +1,29 @@
 class Game < ActiveRecord::Base
   after_create :initialize_board
-  
+
   has_many :pieces
   belongs_to :white_player, class_name: "User"
   belongs_to :black_player, class_name: "User"
-  
+
   def find_piece_icon(x,y)
     #piece = self.pieces.where( :x_position => x, :y_position => y).first
     piece = piece_on(x,y)
     return (!piece.nil?) ? piece.icon.html_safe : ""
   end
-  
+
   def piece_on(x,y)
     return self.pieces.where( :x_position => x, :y_position => y).first
   end
-  
+
   # Initialze board with 16 chess pieces to start the game
   def initialize_board
-    
+
     # Create Pawns
     8.times do |index|
       Pawn.create( :x_position => 1, :y_position => index, :color => 0, :game => self)
       Pawn.create( :x_position => 6, :y_position => index, :color => 1, :game => self)
     end
-    
+
     # Create Rooks, Knights, Bishops
     2.times do |index|
       Rook.create( :x_position => 0, :y_position => index * 6 + index, :color => 0, :game => self)
@@ -33,18 +33,22 @@ class Game < ActiveRecord::Base
       Bishop.create( :x_position => 0, :y_position => index * 3 + 2, :color => 0, :game => self)
       Bishop.create( :x_position => 7, :y_position => index * 3 + 2, :color => 1, :game => self)
     end
-    
+
     # Create Kings and Queens
     Queen.create( :x_position => 0, :y_position => 3, :color => 0, :game => self)
     Queen.create( :x_position => 7, :y_position => 3, :color => 1, :game => self)
     King.create( :x_position => 0, :y_position => 4, :color => 0, :game => self)
     King.create( :x_position => 7, :y_position => 4, :color => 1, :game => self)
-    
+
   end
-  
+
   # For testing
   def clear_board!
     self.pieces.delete_all
   end
-  
+
+  def find_king(type, color)
+    king = self.pieces.where(:type => 'King', :color => color).first
+  end
+
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,24 +2,38 @@ require 'rails_helper'
 require 'piece'
 
 RSpec.describe Game, type: :model do
-  
+
   before do
     @game = Game.create
     @game.clear_board!
   end
-  
+
   it "should find matching piece in the game" do
     bishop = Bishop.create(:x_position => 0, :y_position => 4, :color => 1, :game => @game)
-    
+
     piece = @game.find_piece_icon(0, 4)
     expect(piece).not_to eq("")
   end
-  
+
   it "shouldn't find matching piece in the game" do
     bishop = Bishop.create(:x_position => 0, :y_position => 4, :color => 1, :game => @game)
-    
+
     piece = @game.find_piece_icon(0, 3)
     expect(piece).to eq("")
   end
-  
+
+  it "should know the positions of the kings" do
+    king_1 = King.create(:x_position => 3, :y_position => 3, :type => 'King', :color => 1, :game => @game)
+    king_0 = King.create(:x_position => 5, :y_position => 5, :type => 'King', :color => 0, :game => @game)
+
+    piece_1 = @game.find_king(king_1, 1)
+    piece_0 = @game.find_king(king_0, 0)
+    expect(piece_1).to eq(king_1)
+    expect(piece_1.x_position).to eq(3)
+    expect(piece_1.y_position).to eq(3)
+    expect(piece_0).to eq(king_0)
+    expect(piece_0.x_position).to eq(5)
+    expect(piece_0.y_position).to eq(5)
+  end
+
 end
