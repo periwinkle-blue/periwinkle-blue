7298ce50c9be2ec583f48adfd648bfd60a9c871d
Walley Yang <walleyyang@gmail.com>
2016-01-17 05:10:52 +0000

    In Check Initial

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,29 +1,30 @@
 class Game < ActiveRecord::Base
+  require 'piece'
   after_create :initialize_board
-  
+
   has_many :pieces
   belongs_to :white_player, class_name: "User"
   belongs_to :black_player, class_name: "User"
-  
+
   def find_piece_icon(x,y)
     #piece = self.pieces.where( :x_position => x, :y_position => y).first
     piece = piece_on(x,y)
     return (!piece.nil?) ? piece.icon.html_safe : ""
   end
-  
+
   def piece_on(x,y)
     return self.pieces.where( :x_position => x, :y_position => y).first
   end
-  
+
   # Initialze board with 16 chess pieces to start the game
   def initialize_board
-    
+
     # Create Pawns
     8.times do |index|
       Pawn.create( :x_position => 1, :y_position => index, :color => 0, :game => self)
       Pawn.create( :x_position => 6, :y_position => index, :color => 1, :game => self)
     end
-    
+
     # Create Rooks, Knights, Bishops
     2.times do |index|
       Rook.create( :x_position => 0, :y_position => index * 6 + index, :color => 0, :game => self)
@@ -33,18 +34,81 @@ class Game < ActiveRecord::Base
       Bishop.create( :x_position => 0, :y_position => index * 3 + 2, :color => 0, :game => self)
       Bishop.create( :x_position => 7, :y_position => index * 3 + 2, :color => 1, :game => self)
     end
-    
+
     # Create Kings and Queens
     Queen.create( :x_position => 0, :y_position => 3, :color => 0, :game => self)
     Queen.create( :x_position => 7, :y_position => 3, :color => 1, :game => self)
     King.create( :x_position => 0, :y_position => 4, :color => 0, :game => self)
     King.create( :x_position => 7, :y_position => 4, :color => 1, :game => self)
-    
+
   end
-  
+
   # For testing
   def clear_board!
     self.pieces.delete_all
   end
-  
+
+  # Loop through board to check pieces for valid moves
+  def king_in_check?
+    piece = Piece.new
+
+    for x in 0..7
+      for y in 0..7
+        # Check for piece in database
+        piece = self.pieces.where(:x_position => x, :y_position => y).first
+
+        if !piece.nil?
+          for position in 0..7
+            # Check piece in position for valid moves
+            check_king_horizontal = piece_on(x, position)
+            check_king_vertical = piece_on(position, y)
+            check_king_top_left = piece_on(x - position, y - position)
+            check_king_top_right = piece_on(x - position, y + position)
+            check_king_bottom_left = piece_on(x + position, y - position)
+            check_king_bottom_right = piece_on(x + position, y + position)
+
+            if piece.valid_move?(x, position) ||
+               piece.valid_move?(position, y) ||
+               piece.valid_move?(x - position, y - position) ||
+               piece.valid_move?(x - position, y + position) ||
+               piece.valid_move?(x + position, y - position) ||
+               piece.valid_move?(x + position, y + position)
+
+              # If position is not nil, check to see if its the opposite king
+              if !check_king_horizontal.nil?
+                return true if check_king_horizontal.type == "King" &&
+                        check_king_horizontal.color != piece.color
+
+              elsif !check_king_vertical.nil?
+                return true if check_king_vertical.type == "King" &&
+                        check_king_vertical.color != piece.color
+
+              elsif !check_king_top_left.nil?
+                return true if check_king_top_left.type == "King" &&
+                        check_king_top_left.color != piece.color
+
+              elsif !check_king_top_right.nil?
+                return true if check_king_top_right.type == "King" &&
+                        check_king_top_right.color != piece.color
+
+              elsif !check_king_bottom_left.nil?
+                return true if check_king_bottom_left.type == "King" &&
+                        check_king_bottom_left.color != piece.color
+
+              elsif !check_king_bottom_right.nil?
+                return true if check_king_bottom_right.type == "King" &&
+                        check_king_bottom_right.color != piece.color
+
+              elsif position == 7
+                return false
+              end
+            end
+          end
+        end
+
+      end
+    end
+
+	end # king_in_check?
+
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,24 +2,99 @@ require 'rails_helper'
 require 'piece'
 
 RSpec.describe Game, type: :model do
-  
+
   before do
     @game = Game.create
     @game.clear_board!
   end
-  
+
   it "should find matching piece in the game" do
     bishop = Bishop.create(:x_position => 0, :y_position => 4, :color => 1, :game => @game)
-    
+
     piece = @game.find_piece_icon(0, 4)
     expect(piece).not_to eq("")
   end
-  
+
   it "shouldn't find matching piece in the game" do
     bishop = Bishop.create(:x_position => 0, :y_position => 4, :color => 1, :game => @game)
-    
+
     piece = @game.find_piece_icon(0, 3)
     expect(piece).to eq("")
   end
-  
+
+  # Left this in case needed
+  it "should loop through and check for pieces" do
+    Knight.create(:color => 0, :x_position => 0, :y_position => 0, :game => @game) # &#9816;
+    Pawn.create(:color => 0, :x_position => 1, :y_position => 3, :game => @game) # &#9817;
+    Queen.create(:color => 0, :x_position => 2, :y_position => 5, :game => @game) # &#9813;
+    King.create(:color => 1, :x_position => 4, :y_position => 3, :game => @game) # &#9818;
+    Bishop.create(:color => 1, :x_position => 6, :y_position => 5, :game => @game) # &#9821;
+    Rook.create(:color => 1, :x_position => 7, :y_position => 7, :game => @game) # &#9820;
+
+    for x in 0..7
+      for y in 0..7
+        expect(@game.find_piece_icon(x, y)).to eq("&#9816;") if x == 0 && y == 0
+        expect(@game.find_piece_icon(x, y)).to eq("") if x == 0 && y == 2
+        expect(@game.find_piece_icon(x, y)).to eq("&#9817;") if x == 1 && y == 3
+        expect(@game.find_piece_icon(x, y)).to eq("&#9813;") if x == 2 && y == 5
+        expect(@game.find_piece_icon(x, y)).to eq("&#9818;") if x == 4 && y == 3
+        expect(@game.find_piece_icon(x, y)).to eq("&#9821;") if x == 6 && y == 5
+        expect(@game.find_piece_icon(x, y)).to eq("&#9820;") if x == 7 && y == 7
+      end
+    end
+  end
+
+  it "should check horizontal pieces king" do
+    King.create(:color => 0, :x_position => 2, :y_position => 2, :game => @game)
+    #pawn = Pawn.create(:color => 0, :x_position => 1, :y_position => 0, :game => @game)
+    Rook.create(:color => 1, :x_position => 2, :y_position => 0, :game => @game)
+
+    expect(@game.king_in_check?).to eq(true)
+  end
+
+  it "should check vertical pieces king" do
+    King.create(:color => 0, :x_position => 7, :y_position => 0, :game => @game)
+    #pawn = Pawn.create(:color => 0, :x_position => 1, :y_position => 0, :game => @game)
+    Rook.create(:color => 1, :x_position => 2, :y_position => 0, :game => @game)
+
+    expect(@game.king_in_check?).to eq(true)
+  end
+
+  it "should check top left diagonal pieces for king" do
+    King.create(:color => 0, :x_position => 2, :y_position => 2, :game => @game)
+    #pawn = Pawn.create(:color => 0, :x_position => 1, :y_position => 0, :game => @game)
+    Bishop.create(:color => 1, :x_position => 4, :y_position => 4, :game => @game)
+
+    expect(@game.king_in_check?).to eq(true)
+  end
+=begin
+  it "should check top right diagonal pieces for king" do
+    King.create(:color => 0, :x_position => 2, :y_position => 2, :game => @game)
+    #pawn = Pawn.create(:color => 0, :x_position => 1, :y_position => 0, :game => @game)
+    Bishop.create(:color => 1, :x_position => 4, :y_position => 4, :game => @game)
+
+    expect(@game.king_in_check?).to eq(true)
+  end
+
+  it "should check bottom left diagonal pieces for king" do
+    King.create(:color => 0, :x_position => 2, :y_position => 2, :game => @game)
+    #pawn = Pawn.create(:color => 0, :x_position => 1, :y_position => 0, :game => @game)
+    Bishop.create(:color => 1, :x_position => 4, :y_position => 4, :game => @game)
+
+    expect(@game.king_in_check?).to eq(true)
+  end
+
+  it "should check bottom right diagonal pieces for king" do
+    King.create(:color => 0, :x_position => 2, :y_position => 2, :game => @game)
+    #pawn = Pawn.create(:color => 0, :x_position => 1, :y_position => 0, :game => @game)
+    Bishop.create(:color => 1, :x_position => 4, :y_position => 4, :game => @game)
+
+    expect(@game.king_in_check?).to eq(true)
+  end
+=end
+
+
+
+
+
 end
