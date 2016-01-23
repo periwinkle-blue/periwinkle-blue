0b1834aef0a0139135387a7385028c3653df3dc9
Trevor Bowyer <bowyer@gmail.com>
2016-01-23 20:31:16 +0000

    Added Firebase to trigger page reload for both players

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,16 +2,26 @@ class PiecesController < ApplicationController
   #before_action :is_current_user_turn, :only => [:update]
   #before_action :does_piece_belong_to_current_user, :only => [:update]
   
-  #skip_before_action :verify_authenticity_token
+ 
 
-	def update		
-    # We may want to return a boolean from 'move_to'. If there was an error captured ther, we should not update turn
-		status = current_piece.move_to(params[:piece][:x_position], params[:piece][:y_position])
+	def update	
+    base_uri = 'https://incandescent-torch-3468.firebaseio.com/'
+
+    firebase = Firebase::Client.new(base_uri)
+
+    if !is_current_user_turn
+      status = "invalid_move"
+    else
+		  status = current_piece.move_to(params[:piece][:x_position], params[:piece][:y_position])
+    end
+
     if status == "own_piece"
     	render :json => { status: "own_piece", msg: "Can't take your own piece!" }
     elsif status == "invalid_move"
     	render :json => { status: "invalid_move", msg: "That move is invalid" }
-    else           
+    else   
+     firebase.push("/games/",{:game=> current_game.id.to_s, :x_position => current_piece.x_position, :y_position => current_piece.y_position, :time => Time.now.to_i})   
+     current_game.update_turn     
      render :json => { status: "success" } and return  
 #		render :text => 'Success'
 		end
@@ -23,9 +33,9 @@ class PiecesController < ApplicationController
 		@current_piece = Piece.find(params[:piece_id])
 	end
   
-    def current_game
-      @game ||= Game.find_by_id(params[:game_id])
-    end
+  def current_game
+    @game ||= Game.find_by_id(params[:game_id])
+  end
 
 	def piece_params
 		params.require(:piece).permit(:x_position, :y_position)
@@ -35,7 +45,10 @@ class PiecesController < ApplicationController
   def is_current_user_turn
     if current_game.turn != current_user.id
 #      flash[:alert] = "Please wait for your turn!"
-      render :json => { status: "error", msg: "Please wait for your turn!"  } and return
+      #render :json => { status: "error", msg: "Please wait for your turn!"  } and 
+      return false
+    else
+      return true
     end
   end
     
